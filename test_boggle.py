0bf19a715c9e593b3804df7d0bf91b5a88cafdf3
mariusz79 <mpawlowic@gmail.com>
2019-03-02 11:26:03 +0000

    create make_grid function

--- test_boggle.py
+++ test_boggle.py
@@ -1,8 +1,24 @@
 import unittest
+import boggle
 
-class test_boggle(unittest.TestCase):
-    def test_is_this_thing_on(self):
-        self.assertEqual(1, 1)
+class TestBoggle(unittest.TestCase):
+    """
+    Our test suite for boggle solver
+    """
+    def test_can_create_an_empty_grid(self):
+        """
+        Test to see if we can create an empty grid
+        """
+        grid = boggle.make_grid(0, 0)
+        self.assertEqual(len(grid), 0)
+    
+    def test_grid_size_is_width_times_height(self):
+        """
+        Test is to ensure that the total size of grid
+        is equal to width * height
+        """
+        grid = boggle.make_grid(2, 3)
+        self.assertEqual(len(grid), 2*3)
 
 #code below is used in vscode editor to run test
 if __name__ == '__main__':
