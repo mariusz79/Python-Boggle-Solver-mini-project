84192b4aa1b3d2aacee511a64929c89130c76f5d
mariusz79 <mpawlowic@gmail.com>
2019-03-02 11:06:36 +0000

    first commit

---
+++ test_boggle.py
@@ -0,0 +1,9 @@
+import unittest
+
+class test_boggle(unittest.TestCase):
+    def test_is_this_thing_on(self):
+        self.assertEqual(1, 1)
+
+#code below is used in vscode editor to run test
+if __name__ == '__main__':
+    unittest.main()
