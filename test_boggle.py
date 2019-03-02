741fe09f7c2b2161d4d8836cf3230b5bb6ade7db
mariusz79 <mpawlowic@gmail.com>
2019-03-02 16:10:53 +0000

    add letters to grid

--- test_boggle.py
+++ test_boggle.py
@@ -1,5 +1,6 @@
 import unittest
 import boggle
+from string import ascii_uppercase
 
 class TestBoggle(unittest.TestCase):
     """
@@ -32,6 +33,14 @@ class TestBoggle(unittest.TestCase):
         self.assertIn((1, 1), grid)
         self.assertNotIn((2, 2), grid)
 
+    def test_grid_is_filled_with_letters(self):
+        """
+        Ensure that each of the coordinates in the grid
+        contains letters
+        """
+        grid = boggle.make_grid(2, 3)
+        for letter in grid.values():
+            self.assertIn(letter, ascii_uppercase)
 
 #code below is used in vscode editor to run test
 if __name__ == '__main__':
