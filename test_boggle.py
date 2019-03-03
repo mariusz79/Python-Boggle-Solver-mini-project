889838d865e89b50ae9386c91172aeccf475a7a8
mariusz79 <mpawlowic@gmail.com>
2019-03-03 18:40:02 +0000

    last commit

--- test_boggle.py
+++ test_boggle.py
@@ -2,30 +2,28 @@ import unittest
 import boggle
 from string import ascii_uppercase
 
+
 class TestBoggle(unittest.TestCase):
     """
     Our test suite for boggle solver
     """
+
     def test_can_create_an_empty_grid(self):
         """
         Test to see if we can create an empty grid
         """
         grid = boggle.make_grid(0, 0)
         self.assertEqual(len(grid), 0)
-    
-    def test_grid_size_is_width_times_height(self):
+
+    def test_grid_size_width_times_height(self):
         """
-        Test is to ensure that the total size of grid
+        Test is to ensure that the total size of the grid
         is equal to width * height
         """
         grid = boggle.make_grid(2, 3)
-        self.assertEqual(len(grid), 2*3)
+        self.assertEqual(len(grid), 6)
 
     def test_grid_coordinates(self):
-        """
-        Test to ensure that all coordinates inside 
-        of the grip can be accessed
-        """
         grid = boggle.make_grid(2, 2)
         self.assertIn((0, 0), grid)
         self.assertIn((0, 1), grid)
@@ -87,7 +85,10 @@ class TestBoggle(unittest.TestCase):
         twoLetterWord = 'AB'
         threeLetterWord = 'ABC'
         notThereWord = 'EEE'
-        dictionary = [twoLetterWord, threeLetterWord, notThereWord]
+
+        fullwords = [twoLetterWord, threeLetterWord, notThereWord]
+        stems = ['A', 'AB', 'E', 'EE']
+        dictionary = fullwords, stems
 
         foundWords = boggle.search(grid, dictionary)
 
@@ -102,6 +103,3 @@ class TestBoggle(unittest.TestCase):
         """
         dictionary = boggle.get_dictionary('words.txt')
         self.assertGreater(len(dictionary), 0)
-
-
-
